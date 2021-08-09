a66fb533787291039f90232de476dbd6746e714c
GZ4003 <you@example.com>
2021-08-09 16:56:40 -0300

    a

---
+++ main.js
@@ -0,0 +1,94 @@
+//PRATICAS DE FOR
+// const forloop = () => {
+//     for(let i= 1; i < 11; i++) {
+//         console.log(i)
+//     }
+// }
+
+// forloop()
+
+// const forloop = () => {
+//     for(let e = 5; e > 1; e++){
+//         console.log(e)
+//     }
+// }
+
+// forloop();
+
+// const forloop = () => {
+//     for(let f = 2; f < 22; f++){
+//         console.log(f)
+//     }
+// }
+
+// forloop();
+
+// const datos = () => {
+//     for(let c = 3; c < 6; c++){
+//         console.log(c)
+//     }
+// }
+
+// datos();
+
+// const pruebas = () => {
+//     for(let t = 5; t < 25; t++){
+//         console.log(t)
+//     }
+// }
+
+// pruebas();
+
+// const tornillos = () => {
+//     for(let h = 4; h < 24; h++){
+//         console.log(h)
+//     }
+// }
+
+// tornillos();
+
+// const tuercas = () => {
+//     for(let u = 40; u > 10; u++){
+//         console.log(u)
+//     }
+// }
+
+// tuercas();
+
+// const maderas = () => {
+//     for(let m = 50; m > 25; m++){
+//         console.log(m)
+//     }
+// }
+
+// maderas();
+
+// const person = {
+//     'name' :  'Alexis Gomez',
+//     'age' :  22,
+//     'dirección' : 'Buenos Aires',
+//     'carrer' : 'Software enginner',
+//     'HomeBanking' : {
+//         'banco' : 'itau'
+//     }
+// , 
+// saludar: () => {
+//     console.log('hola')
+// }
+
+// }
+// console.log(person.name) 
+
+const person = {
+    'name': 'gonzalo pinto',
+    'age' : 21,
+    'dirección' : 'buenos aires',
+    'carrer': 'medicina',
+
+    saludar: () => {
+         console.log('hola')
+    
+    }
+}
+
+console.log(person.name)
